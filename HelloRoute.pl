9be0fdc9ef4db745c709770ee2df224571502caf
Madeleine Francis <madeleinefrncs@gmail.com>
2017-10-23 19:28:18 -0700

    Added vancouver map facts to create graph

--- HelloRoute.pl
+++ HelloRoute.pl
@@ -24,6 +24,81 @@ edge(g,d,10).
 edge(f,d,2).
 edge(f,g,3).
 
+edge(ubc,vandusen,39, 126, 16, 50).
+edge(vandusen,ubc,39, 126, 16, 50).
+
+edge(kits,ubc,27,94,15,34).
+edge(ubc,kits,27,94,15,34).
+
+edge(ubc,vgh,39,115,17,44).
+edge(vgh,ubc,39,115,17,44).
+
+edge(vgh,kits,21,48,12,32).
+edge(kits,vgh,21,48,12,32).
+
+edge(vandusen,vgh,15,36,6,12).
+edge(vgh,vandusen,15,36,6,12).
+
+edge(oakridge,vandusen,7,18,3,12).
+edge(vandusen,oakridge,7,18,3,12).
+
+edge(vgh,oakridge,17,43,10,16).
+edge(oakridge,vgh,17,43,10,16).
+
+edge(granville,vgh,9,32,6,20).
+edge(vgh,granville,9,32,6,20).
+
+edge(kits,granville,8,25,11,22).
+edge(granville,kits,8,25,11,22).
+
+edge(stPauls,kits,12,32,7,11).
+edge(kits,stPauls,12,32,7,11).
+
+edge(granville,stPauls,12,19,9,21).
+edge(stPauls,granville,12,19,9,21).
+
+edge(stPauls,englishbay,11,22,6,16).
+edge(englishbay,stPauls,11,22,6,16).
+
+edge(englishbay,stanley,12,26,8,18).
+edge(stanley,englishbay,12,26,8,18).
+
+edge(stanley,cap,12,89,15,40).
+edge(cap,stanley,12,89,15,40).
+
+edge(cap,stPauls,40,109,20,38).
+edge(cap,stPauls,40,109,20,38).
+
+edge(waterfront,stPauls,11,22,8,14).
+edge(stPauls,waterfront,11,22,8,14).
+
+edge(rogers,waterfront,5,14,5,5).
+edge(waterfront,rogers,5,14,5,5).
+
+edge(waterfront,lynn,58,123,26,69).
+edge(lynn,waterfront,58,123,26,69).
+
+edge(lynn,commercial,74,193,23,74).
+edge(commercial,lynn,74,193,23,74).
+
+edge(commercial,troutLake,6,16,4,13).
+edge(troutLake,commercial,6,16,4,13).
+
+edge(main,commercial,12,37,6,3).
+edge(commercial,main,12,37,6,3).
+
+edge(commercial,vgh,23,54,10,21).
+edge(vgh,commercial,23,54,10,21).
+
+edge(main,rogers,8,14,3,3).
+edge(rogers,main,8,14,3,3).
+
+edge(vgh,main,15,34,9,26).
+edge(main,vgh,15,34,9,26).
+
+edge(rogers,granville,16,36,9,22).
+edge(granville,rogers,16,36,9,22).
+
 nodes([a,b,c,d,e,f,g]).
 
 adjacentNodes(S, E) :- findall(Y, edge(S,Y,_), E). 
@@ -50,6 +125,7 @@ min_in_list([H,K|T],M) :-
     min_in_list([K|T],M).               
 
 
+%pass in bus,bike,or walk into Variable, Mode. 
 dijks(A,ShPaths) :- 
 	nodes(Nodes),
 	setInfinite(Nodes,A),
@@ -59,7 +135,8 @@ dijks(A,ShPaths) :-
 
 	assert(shortest_path(A,[A],0)),
 	assert(to_handle(A)),
-	loop,
+	%change loop to take in Mode parameter 
+	loop(Mode),
 	
 	setof(shortest_path(X,L,D),shortest_path(X,L,D),ShPaths).
 
@@ -69,25 +146,28 @@ setInfinite([H|T],A) :-
 	assert(free(H)),
 	setInfinite(T,A).
 
-loop :-
+%loop will use mode parameter 
+loop(Mode) :-
 	retract(to_handle(A)),
 	adjacentNodes(A,E),
 	assert(adjacent(A, E)),
-	handle(A,E),
-	loop.
-loop.
+	%pass mode parameter here, 
+	handle(A,E,Mode),
+	loop(Mode).
+loop(Mode).
 
-handle(A,[]).
-handle(A,[H|T]) :-
+%pass mode into handle function
+handle(A,[],Mode).
+handle(A,[H|T],Mode) :-
 	retract(free(H)),
 	shortest_path(A,Path,Dist),
 	findEdgeWeight(A,H,W),
 	Dist1 is Dist + W,
 	assert(shortest_path(H,[H|Path],Dist1)),
 	assertz(to_handle(H)),
-	handle(A,T).
+	handle(A,T,Mode).
 
-handle(A,[_|T]) :- handle(A,T).
+handle(A,[_|T],Mode) :- handle(A,T,Mode).
 
 
 p(A) :- adjacentNodes(A,E)),
